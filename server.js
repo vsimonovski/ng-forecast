caae5a3bd79f7d9c879bc67936c0c0d156ecfe9c
Vladimir Simonovski <vlsim@outlook.com>
2017-01-26 19:12:14 +0100

    FIX: Responsive, minor bugs

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ var port = '4000';
 app.use(express.static(__dirname + '/dist/'));
 app.listen(port);
 
-console.log('Application active on: http://localhost:4000');
+console.log('Application active on: http://localhost:' + port);
